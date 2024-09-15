7d0091ebc1df914a661ecc302977569d6c749bc2
dawid-platek <dawidplatek.eu.pl@outlook.com>
2024-09-15 17:11:03 +0200

    Module 1 - program (Trapezoid_area)

---
+++ 03_trapezoid_area.py
@@ -0,0 +1,7 @@
+print('Obliczenie pola trapezu')
+a = float(input('Podaj długość podstawy boku A w cm: '))
+b = float(input('Podaj długość podstawy boku B w cm: '))
+h = float(input('Podaj wysokość trapezu (H) w cm: '))
+area = (((a+b)*h)/2)
+print(f'Pole trapezu wynosi: {area}')
+
